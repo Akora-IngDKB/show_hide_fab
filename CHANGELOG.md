a19a2d83b8ee17e319cac6389b4002c192318474
Akora-IngDKB <debrahkwesibuabeng2@gmail.com>
2020-04-18 02:55:22 +0000

    Edited LICENSE, CHANGELOG; Wrote package code

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
-## [0.0.1] - TODO: Add release date.
+## [0.0.1] - 2020-04-18.
 
-* TODO: Describe initial release.
+* Package description for easy usage.
--- lib/show_hide_fab.dart
+++ lib/show_hide_fab.dart
@@ -1,7 +1,31 @@
 library show_hide_fab;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class ShowHideFAB extends StatelessWidget {
+  final Duration animationDuration;
+  final FloatingActionButton fab;
+  bool shouldShow;
+
+  ShowHideFAB({
+    @required this.fab,
+    this.animationDuration = kThemeAnimationDuration,
+    this.shouldShow = true,
+  }) {
+    assert(fab != null, 'fab cannot be null');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSwitcher(
+      duration: animationDuration,
+      child: shouldShow ? fab : Offstage(),
+      transitionBuilder: (child, animation) {
+        return ScaleTransition(
+          scale: animation,
+          child: child,
+        );
+      },
+    );
+  }
 }
