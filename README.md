9f4673da00f087012a324e3f1d30ef46a42fc0dd
Akora-IngDKB <debrahkwesibuabeng2@gmail.com>
2020-04-18 04:24:22 +0000

    Added Example demo; Updated Readme

--- README.md
+++ README.md
@@ -1,14 +1,6 @@
 # show_hide_fab
 
-A new Flutter package project.
+A flutter package that provides easy methods to show and hide a FAB attached to a Scaffold.  
+<img src="example/demo.gif" height=600em><br />  
 
-## Getting Started
-
-This project is a starting point for a Dart
-[package](https://flutter.dev/developing-packages/),
-a library module containing code that can be shared easily across
-multiple Flutter or Dart projects.
-
-For help getting started with Flutter, view our 
-[online documentation](https://flutter.dev/docs), which offers tutorials, 
-samples, guidance on mobile development, and a full API reference.
+Check implementation in the Example tab.
---
+++ example/lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:show_hide_fab/show_hide_fab.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'ShowHideFAB Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: MyHomePage(title: 'ShowHideFAB Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+
+  MyHomePage({this.title});
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool show = true;
+  String text = 'HIDE FAB';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            setState(() {
+              show = !show;
+              text = 'SHOW FAB';
+            });
+          },
+          color: Colors.green,
+          child: Text(text),
+        ),
+      ),
+      floatingActionButton: ShowHideFAB(
+        shouldShow: show,
+        animationDuration: Duration(milliseconds: 500),
+        fab: FloatingActionButton(
+          backgroundColor: Colors.green,
+          child: Icon(Icons.add, color: Colors.white),
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
+}
