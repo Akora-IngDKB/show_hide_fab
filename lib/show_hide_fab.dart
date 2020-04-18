c2a8374b61d566ff8e449922a1b9f537da3b26d4
Akora-IngDKB <debrahkwesibuabeng2@gmail.com>
2020-04-18 03:18:02 +0000

    Documented code

--- lib/show_hide_fab.dart
+++ lib/show_hide_fab.dart
@@ -3,12 +3,26 @@ library show_hide_fab;
 import 'package:flutter/material.dart';
 
 class ShowHideFAB extends StatelessWidget {
+  /// The duration for the animation
+  /// Defaults to the app's theme duration.
   final Duration animationDuration;
+
+  /// The floating action button widget to show/hide.
   final FloatingActionButton fab;
+
+  /// Curve to apply when [fab] is coming into view.
   final Curve showCurve;
+
+  /// Curve to apply when [fab] is disappearing from view.
   final Curve hideCurve;
+
+  /// boolean to toggle show/hide [fab]
   bool shouldShow;
 
+  /// Creates a [ShowHideFAB] widget.
+  ///
+  /// [fab] parameter must not be null.
+  /// [shouldShow] parameter toggles the [fab] (true/false => show/hide).
   ShowHideFAB({
     @required this.fab,
     this.animationDuration = kThemeAnimationDuration,
